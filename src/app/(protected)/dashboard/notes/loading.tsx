96f95f6899af1e04c91192dc11ed50cb17a4d93b
Sajjad Shaikh <samaralishaikh212@gmail.com>
2025-05-04 20:38:45 +0530

    added loading screen

---
+++ src/app/(protected)/dashboard/notes/loading.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function NotesLoading() {
+  return (
+    <>
+      <Skeleton className="h-18 w-full rounded-lg mb-4" />
+      <Skeleton className="h-10 w-1/2 rounded-lg mb-4" />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <Skeleton className="h-72 w-full rounded-lg" />
+        <Skeleton className="h-72 w-full rounded-lg" />
+        <Skeleton className="h-72 w-full rounded-lg" />
+        </div>
+    </>
+  );
+}
